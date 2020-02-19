5f1e141156b6aa2796763fee763788320bed9940
Babalakin Oyeyemi <oyeyemi.ola@gmail.com>
2020-02-19 11:55:41 +0000

    Scala exercises up till iteration 2

---
+++ src/main/scala/FirstWorksheet.sc
@@ -0,0 +1,21 @@
+val name = "Babalakin"
+val surname = " Oyeyemi"
+val text = "peter,picks,peppers"
+val height = 1.86
+val nums = Array(1,2,3,4,5,6,7,8)
+println(s"Hi my name is $name")
+println(f"and I am $height%2.2fm tall")
+println("escaping to \n new line")
+println(raw"test \n line")
+
+// String methods
+println(name concat surname)
+println(surname charAt 7)
+println(surname endsWith "yemi")
+println(surname indexOf "y")
+println(surname.replace("e", "a"))
+val array = text split ","
+println(array.length)
+println(nums.length)
+println(name.substring(4,8))
+println(surname.trim())
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,139 @@
+class Person(var name: String, var age: Int)
+
 object Main {
 
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
+
+    /*
+    Basic Exercises
+     */
+    // Hello World!
     println("Hello World!")
+
+    // Assignment
+    var text = "Hello World!"
+    println(text)
+
+    // Parameters
+    helloWorld("something")
+
+    // Return types
+    println(foo("Hello World!"))
+
+    // Type inference
+    newFoo(2.1)
+    newFoo("some text")
+    newFoo(5)
+    newFoo(true)
+
+    // Strings
+    println(endOfStrnig("Hello", 3))
+
+    // Strings 2
+    println(fourParamStringMethod("Ha", "llo", 'a', 'e'))
+
+    // Operators
+    println(operatorMethod(2, 3))
+
+    // Conditionals
+    println(operatorMethod2(2, 5, bool = true))
+
+    // Conditionals 2
+    println(operatorMethod3(0, 0, bool = true))
+
+    // Iteration
+    iterationMethod("Hi", 3)
+
+    // Iteration 2
+
+    // Iteration 3
+
+    // Iteration 4
+
+    // Pattern Matching 1
+
+    // Pattern Matching 2
+
+    // Functional 1
+
+    /*
+    Intermediate Exercises
+    */
   }
+
+  def helloWorld(text: String): Unit = {
+    println(text)
+  }
+
+  def foo(text: String): String = {
+    text
+  }
+
+  def newFoo(thing: Any): Unit = {
+    println(thing)
+  }
+
+  2
+
+  def endOfStrnig(word: String, index: Int): String = {
+    word.substring(word.length - index)
+  }
+
+  def fourParamStringMethod(word1: String, word2: String, char1: Char, char2: Char): String = {
+    val newWord = word1 concat word2
+    println(newWord)
+    newWord.replace(char1, char2)
+  }
+
+  def operatorMethod(x: Int, y: Int): Int = x + y
+
+  def operatorMethod2(x: Int, y: Int, bool: Boolean): Int = {
+    if (!bool) {
+      return x * y
+    }
+
+    x + y
+  }
+
+  def operatorMethod3(x: Int, y: Int, bool: Boolean): Int = {
+    if (x == 0 && y == 0)
+      return 0
+
+    if (x == 0)
+      return y
+
+    if (y == 0)
+      return x
+
+    if (!bool) {
+      return x * y
+    }
+
+    x + y
+  }
+
+  def iterationMethod(word: String, num: Int): Unit = {
+    for (i <- 1 to num)
+      println(word)
+  }
+
+  def iterationMethod2(word: String, num: Int): Unit = {
+    /*
+    Unfinished
+
+    create an empty string, for the length of num, concat the string onto the existing string val
+    when num has been reached create new line and repeat for has many values num has.
+
+    i = how many rows and j = how many items in row
+     */
+
+
+    for (i <- 1 to num; j <- 1 to num) {
+      var emptyString = ""
+      print()
+    }
+
+  }
+
 
 }
